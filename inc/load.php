2070b4db43617f743762fd7b3c77a9d3cf0e6a15
zmatlja1 <zisanek@gmail.com>
2011-05-10 20:07:35 +0200

    Added RemoteApi classes calls into load.php.

--- inc/load.php
+++ inc/load.php
@@ -11,7 +11,8 @@ spl_autoload_register('load_autoload');
 // require all the common libraries
 // for a few of these order does matter
 require_once(DOKU_INC.'inc/RemoteAPI/IRemoteAPI.php');
-require_once(DOKU_INC.'inc/RemoteAPI/RemoteAPI.php');
+require_once(DOKU_INC.'inc/RemoteAPI/RPCServer.php');
+require_once(DOKU_INC.'inc/RemoteAPI/RPCServerFactory.php');
 
 require_once(DOKU_INC.'inc/blowfish.php');
 require_once(DOKU_INC.'inc/actions.php');
